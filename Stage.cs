0ff4de238bba6d52aa70ab202e959dddf286c02d
Peter Toth <peter.toth@eon-hungaria.com>
2021-04-07 20:15:38 +0200

    Font color and style properties added to Stage

--- Stage.cs
+++ Stage.cs
@@ -27,11 +27,13 @@ namespace BpToolsWPFClientTest
         public string Description { get { return bpStage.Description; } }
         public string BpFontFamily { get { return bpStage.Font.Family; } }
         public int BpFontSize { get { return bpStage.Font.Size; } }
+        public Brush BpFontColor { get { return (SolidColorBrush)new BrushConverter().ConvertFrom("#" + bpStage.Font.Color); } }
+        public string BpFontStyle { get { return bpStage.Font.Style; } }
         public TransformGroup Transforms { get; } = new TransformGroup();
         public TranslateTransform StageTranslate { get; } = new TranslateTransform();
 
         public Stage(BPT.Stage stage)
-        {
+        {            
             bpStage = stage;
             this.Height = bpStage.Height;
             this.Width = bpStage.Width;
@@ -39,8 +41,9 @@ namespace BpToolsWPFClientTest
             StageTranslate.Y = bpStage.Y - bpStage.Height / 2;
             Transforms.Children.Add(StageTranslate);
             this.RenderTransform = Transforms;
-            
+
             this.DataContext = this;
         }
+
     }
 }
